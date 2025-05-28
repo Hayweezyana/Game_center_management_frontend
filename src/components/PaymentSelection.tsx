832bca05a07dfd12bddf0aae34ea722c86c071b0
hayweezyana <ayotundeadeboyeje@gmail.com>
2025-05-28 21:23:15 +0100

    payment selection logos

--- src/components/PaymentSelection.tsx
+++ src/components/PaymentSelection.tsx
@@ -1,6 +1,7 @@
 // PaymentSelection.tsx
 import React from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
+import './PaymentSelection.css'
 
 interface PaymentSelectionProps {
   cartTotal: number;
